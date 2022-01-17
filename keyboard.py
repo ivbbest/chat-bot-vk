7edf0ce04e43b210419acae606f26c302ce7ea76
Vladimir Irkhin <ivb1989@yandex.ru>
2022-01-17 17:05:19 +0300

    Create def create template in carousel product.

--- keyboard.py
+++ keyboard.py
@@ -1,5 +1,31 @@
 import json
 from settings import MAX_BUTTONS_ON_LINE, MAX_INLINE_LINES, MAX_DEFAULT_LINES
+import db
+import sys
+
+
+# создание шаблона для карусели товаров
+def create_template_carousel(category):
+    try:
+        products = db.select_all_menu(category)
+        product_info = list()
+        for product in products:
+            title, description, photo_id = product
+            all_element = dict([('buttons', [{
+                "action": {
+                    "type": "text",
+                    "label": "Заказать"
+                }
+            }]), ('description', description),
+                                ('photo_id', photo_id), ('title', title)
+                                ])
+            product_info.append(all_element)
+
+        template = dict([("type", "carousel"), ("elements", product_info)])
+        # breakpoint()
+        return json.dumps(template)
+    except Exception as e:
+        print('Error template', type(e))
 
 
 class VkKeyboard:
