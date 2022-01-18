d9f5b6c377c4cd5af98c1af6294878209ad1576d
Vladimir Irkhin <ivb1989@yandex.ru>
2022-01-18 19:13:58 +0300

    Add README.md. Fix small bugs. Add docstring.

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
-import sys
-from settings import token, version, group_id
+from config import version, group_id, token
 from vk import VKLongPoll
 from keyboard import VkKeyboard
 from template_carousel import create_template_carousel
@@ -8,6 +7,9 @@ import db
 
 
 def create_keyword():
+    """
+    Получение всех категорий и использования для анализа в чат-боте, что выбирает пользователь
+    """
     keyword = db.select_all_category()
 
     return [key.lower() for key in keyword]
@@ -19,53 +21,42 @@ def main():
 
     for event in vk.listen():
         if event['type'] == 'message_new':
-            print(event)
-            print()
             user_id = int(event['object']['message']['from_id'])
             body = event['object']['message']['text']
             name_user = vk.users_name_get(user_id)
 
             choice_keyboard = VkKeyboard()
             choice_keyboard.create_keyboard(menu=False)
-            vk.send_message(user_id, f'{name_user}, привет. Тебе показать наше меню?', keyboard=choice_keyboard.get_keyboard(),
+            vk.send_message(user_id, f'{name_user}, привет. Тебе показать наше меню?',
+                            keyboard=choice_keyboard.get_keyboard(),
                             random_id=randint(0, 100))
 
             if body.lower() == 'да':
-                print('Попал в блок Да')
-                print()
-
                 menu_keyboard = VkKeyboard()
                 menu_keyboard.create_keyboard()
-
-                vk.send_message(user_id, 'Вот наша продукция. Выбирай;-)', keyboard=menu_keyboard.get_keyboard(),
+                vk.send_message(user_id, 'Вот наша продукция. Выбирай;-)',
+                                keyboard=menu_keyboard.get_keyboard(),
                                 random_id=randint(0, 100))
 
             elif body.lower() == 'назад':
-                    print('Попал в блок Назад')
-                    print()
-                    vk.send_message(user_id, '')
-                    continue
-
-            elif body.lower() in keywords:
-                    print('Попал в блок категории')
-                    print()
-                    vk.send_message_carousel(user_id, f'Выбрал категорию {body.capitalize()}',
-                                             template=create_template_carousel(body),
-                                             random_id=randint(0, 100))
-                    vk.send_message(user_id, 'Еще вот что у нас в меню',
-                                    keyboard=menu_keyboard.get_keyboard(),
-                                    random_id=randint(0, 100))
-
-            elif body.lower() == 'нет':
-                print('Попал в блок Нет')
-                print()
-                vk.send_message(user_id, 'На нет и суда нет;-)', random_id=randint(0, 100))
+                vk.send_message(user_id, '')
                 continue
 
+            elif body.lower() in keywords:
+                vk.send_message_carousel(user_id, f'Выбрал категорию {body.capitalize()}',
+                                         template=create_template_carousel(body),
+                                         random_id=randint(0, 100))
+                vk.send_message(user_id, 'Еще вот что у нас в меню',
+                                keyboard=menu_keyboard.get_keyboard(),
+                                random_id=randint(0, 100))
+
+            elif body.lower() == 'нет':
+                vk.send_message(user_id, 'На нет и суда нет;-)',
+                                random_id=randint(0, 100))
+                continue
             elif body.lower() == 'завершить':
-                print('Попал в блок Завершить')
-                print()
-                vk.send_message(user_id, 'С тобой было очень приятно. Пока;-)', random_id=randint(0, 100))
+                vk.send_message(user_id, f'{name_user}, c тобой было очень приятно. Пока;-)',
+                                random_id=randint(0, 100))
                 break
 
         else:
